21d4b86688043dcb67a45b91f195abae3db72475
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2021-01-16 14:12:29 +0100

    :heavy_minus_sign: Drop dependency on bash

--- autoblockchainify/run-autoblockchainify.sh
+++ autoblockchainify/run-autoblockchainify.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -e
+#!/bin/sh -e
 # Docker runner
 
 # This needs to go *into* the volume, which is probably mapped by
 # docker-compose. So at start only… (as opposed to image creation)
-if [[ ! -d /blockchain/.gnupg/ ]]; then
+if [ ! -d /blockchain/.gnupg/ ]; then
   gpg1 --pgp2 --import /root/stamper.asc
 fi
 exec autoblockchainify "$@"
